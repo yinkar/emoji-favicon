b0bb74fe7354f92a864a6c15cc61d67c3eaeb0a2
Yinkar <hkrabs@yandex.com>
2022-02-12 23:24:23 +0300

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     const [outputCode, setOutputCode] = useState(``);
     const [faviconHref, setFaviconHref] = useState(``);
 
-    const [emojiList, setEmojiList] = useState([]);
-    const [emojiJson, setEmojiJson] = useState({});
+    let emojiList = [];
+    let emojiJson = {};
     
     const faviconTemplate = `<link rel="icon" href="{href}" type="image/svg+xml" />`;
     const faviconHrefTemplate = `data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20{width}%20{height}'%3E%3Ctext%20x='0'%20y='14'%3E{emoji}%3C/text%3E%3C/svg%3E`;
@@ -29,8 +29,26 @@ function App() {
         return arr[random(0, arr.length)];
     }
 
+    const setEmojiData = e => {
+        const newEmoji = e;
+
+        setEmoji(newEmoji);
+
+        const newFaviconHref = faviconHrefTemplate
+            .replace(`{emoji}`, newEmoji)
+            .replace(`{width}`, dimensions.width)
+            .replace(`{height}`, dimensions.height);
+
+        setFaviconHref(newFaviconHref);
+
+        setOutputCode(faviconTemplate
+            .replace(`{href}`, newFaviconHref));
+
+        faviconElement.setAttribute('href', faviconHref);
+    };
+
     const emojiClick = e => {
-        setEmoji(e.detail.unicode);
+        setEmojiData(e.detail.unicode);
     };
         
     const copyCode = () => {
@@ -48,9 +66,7 @@ function App() {
         faviconHref,
         setFaviconHref,
         emojiList,
-        setEmojiList,
         emojiJson,
-        setEmojiJson,
         faviconElement,
         random,
         randomInArray,
@@ -62,29 +78,13 @@ function App() {
         fetch('https://unpkg.com/unicode-emoji-json@0.3.0/data-by-emoji.json')
         .then(res => res.json())
         .then(data => {
-            setEmojiJson(data);
+            emojiJson = data;
 
-            setEmojiList(Array.from(Object.keys(emojiJson)));
+            emojiList = Array.from(Object.keys(emojiJson));
 
-            setEmoji(randomInArray(emojiList));
+            setEmojiData(randomInArray(emojiList));
         });
     }, []);
-
-    useEffect(() => {
-        setFaviconHref(faviconHrefTemplate
-            .replace(`{emoji}`, emoji)
-            .replace(`{width}`, dimensions.width)
-            .replace(`{height}`, dimensions.height));
-
-        setOutputCode(faviconTemplate
-            .replace(`{href}`, faviconHref));
-
-        faviconElement.setAttribute('href', faviconHref);
-    }, [emoji]);
-
-    useEffect(() => {
-        
-    }, [outputCode]);
 
     return (
         <MainContext.Provider value={data}>
