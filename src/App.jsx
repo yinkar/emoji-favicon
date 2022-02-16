858920c781a4fca3ee9fa3ef6e6e79774ad2f57a
Yinkar <hkrabs@yandex.com>
2022-02-16 16:04:18 +0300

    Optimize

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import 'emoji-picker-element';
 import { MainContext } from './context';
 import Content from './components/Content';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
     const [emoji, setEmoji] = useState(`⏳`);
     const [outputCode, setOutputCode] = useState(``);
     const [faviconHref, setFaviconHref] = useState(``);
 
-    let emojiList = [];
-    let emojiJson = {};
+    let emojiList = useRef([]);
+    let emojiJson = useRef({});
     
     const faviconTemplate = `<link rel="icon" href="{href}" type="image/svg+xml" />`;
     const faviconHrefTemplate = `data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20{width}%20{height}'%3E%3Ctext%20x='0'%20y='14'%3E{emoji}%3C/text%3E%3C/svg%3E`;
@@ -50,23 +50,17 @@ function App() {
     };
         
     const copyCode = () => {
-        navigator.clipboard.writeText(outputCode);        
+        const faviconCodeArea = document.querySelector('#favicon-code');
+        faviconCodeArea.select();
+        faviconCodeArea.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(faviconCodeArea.value);        
     };
 
     const data = {
-        faviconTemplate,
-        faviconHrefTemplate,
-        dimensions,
         emoji,
         setEmoji,
         outputCode,
         setOutputCode,
-        faviconHref,
-        setFaviconHref,
-        emojiList,
-        emojiJson,
-        random,
-        randomInArray,
         emojiClick,
         copyCode
     };
@@ -75,11 +69,11 @@ function App() {
         fetch('https://unpkg.com/unicode-emoji-json@0.3.0/data-by-emoji.json')
         .then(res => res.json())
         .then(data => {
-            emojiJson = data;
+            emojiJson.current = data;
 
-            emojiList = Array.from(Object.keys(emojiJson));
+            emojiList.current = Array.from(Object.keys(emojiJson.current));
 
-            setEmojiData(randomInArray(emojiList));
+            setEmojiData(randomInArray(emojiList.current));
         });
     }, []);
 
